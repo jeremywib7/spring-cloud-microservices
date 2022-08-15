69e166bd2b7a9261711f40e68cb2e0caddc1e084
Jeremy <jeremywib7@gmail,com>
2022-08-15 22:35:52 +0700

    good job

--- fraud/src/main/java/com/joy/fraud/FraudController.java
+++ fraud/src/main/java/com/joy/fraud/FraudController.java
@@ -1,14 +1,22 @@
 package com.joy.fraud;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@AllArgsConstructor
 public class FraudController {
-    @PostMapping(path = "{customerId}")
-    public FraudCheckResponse isFraudster() {
 
-    } 
+    private final FraudCheckService fraudCheckService;
+
+    @PostMapping(path = "{customerId}")
+    public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
+        boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        return new FraudCheckResponse(isFraudulentCustomer);
+    }
 }
