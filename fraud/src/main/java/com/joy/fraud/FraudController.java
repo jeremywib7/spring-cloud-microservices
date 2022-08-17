cfb6e5eda3dd3519848649bf962ca9616239257c
Jeremy Yonathan <jeremywib7@gmail.com>
2022-08-17 18:03:58 +0700

    fix amburadul

--- fraud/src/main/java/com/joy/fraud/FraudController.java
+++ fraud/src/main/java/com/joy/fraud/FraudController.java
@@ -1,19 +1,12 @@
 package com.joy.fraud;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-import com.FraudCheckResponse;
-
->>>>>>> 9b6c573da219628c992e5c63ef210f74fd691fb7
->>>>>>> 428c6ea8859ed92454e2ab46b6dbaf10ef33e0e8
 import lombok.AllArgsConstructor;
+
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
@@ -21,19 +14,11 @@ import lombok.AllArgsConstructor;
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
-<<<<<<< HEAD
 
     @PostMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
-=======
-    
-    @GetMapping(path = "{customerId}")
-    public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId ) {
-        boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
-        return new FraudCheckResponse(isFraudulentCustomer);
-    } 
->>>>>>> 9b6c573da219628c992e5c63ef210f74fd691fb7
+
 }
