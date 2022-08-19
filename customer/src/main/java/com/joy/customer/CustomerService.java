0c4b10c4981314c8a4399f5364ca050b85dc8fac
Jeremy Yonathan <jeremywib7@gmail.com>
2022-08-19 17:01:51 +0700

    configure eureka server

--- customer/src/main/java/com/joy/customer/CustomerService.java
+++ customer/src/main/java/com/joy/customer/CustomerService.java
@@ -23,7 +23,7 @@ public class CustomerService {
     customerRepository.saveAndFlush(customer);
     // TODO check if fraudster
     FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-        "http://localhost:8079/api/v1/fraud-check/{customerId}",
+        "http://FRAUD/api/v1/fraud-check/{customerId}",
         FraudCheckResponse.class,
         customer.getId());
 
@@ -32,7 +32,6 @@ public class CustomerService {
     }
 
     // TODO send notification
-    
 
   }
 }
